d2902b2e69b4178c650f09158b858524930f1e5f
Giovanni Lawrence <isekcftw@gmail.com>
2023-11-07 07:31:44 -0400

    Added initial starter files

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
 /* Add your JavaScript to this file */
+
+window.onload = function(){
+    let subscribeBtn = document.querySelectorAll(".btn")[1];
+        
+    subscribeBtn.addEventListener('click',
+    function (e){
+        e.preventDefault();
+    
+        var email = document.getElementById("email").value;
+    
+        if(email != ""){
+            document.getElementsByClassName("message")[0].innerHTML = "Thank You! Your email address " +email+ " has been added to our mailing list";
+        }else{
+            document.getElementsByClassName("message")[0].innerHTML = "Please enter an email address"
+        }
+    });
+}
